144c348d45220421820a2b63d97602624500e1d7
Matt Carter <mdcarter@unm.edu>
2014-10-23 12:36:21 -0600

    reformatted json tweaks to support CORS move to top code added test code for edge detection for drop down menus.

--- src/js/unm-scripts.js
+++ src/js/unm-scripts.js
@@ -1,125 +1,189 @@
 /* Move menu to top in collapse state.
-***********************/
+ ***********************/
 sizeStatus = 'big';
 breakpt = 752;
 
 if ($(window).width() < breakpt) {
-    moveMenu();
-}
-else {
-    sizeStatus = 'big';
+	moveMenu();
+} else {
+	sizeStatus = 'big';
 }
 
-$( document ).ready(function() {
-	moveMenu(); // run initial to see if screen is small already
-    $(window).resize(function() {moveMenu(); });        
+$(document).ready(function() {
+	moveMenu();
+	// run initial to see if screen is small already
+	$(window).resize(function() {
+		moveMenu();
+	});
 });
 
+function moveMenu() {
+	currentSize = $(window).width();
+	if (currentSize < breakpt && sizeStatus == 'big') {
 
-   function moveMenu() {
-            currentSize = $(window).width(); 
-            if (currentSize < breakpt && sizeStatus == 'big') {
+		$('.navbar-collapse').each(function(index) {
+			$(this).addClass('menu' + index);
+			$(this).parent().addClass('menu' + index + '-parent');
+			$(this).detach().appendTo('#mobile-menu');
+			sizeStatus = 'small';
+			$(this).find('a.dropdown-toggle').attr('data-toggle', 'no-dropdown');
 
-                $('.navbar-collapse').each(function(index) {
-                    $(this).addClass('menu'+index);  
-                    $(this).parent().addClass('menu'+index+'-parent');                
-                    $(this).detach().appendTo('#mobile-menu');
-                    sizeStatus = 'small';
-                    $(this).find('a.dropdown-toggle').attr('data-toggle', 'no-dropdown');
-                    
-                });
-            }
-            else if (currentSize > breakpt && sizeStatus == 'small') {
-                                 
-                $('#mobile-menu .navbar-collapse').each(function(index) {
-                    $(this).removeClass('menu'+index);                 
-                    $(this).detach().appendTo('.menu'+index+'-parent');
-                    $(this).parent().removeClass('menu'+index+'-parent'); 
-                    sizeStatus = 'small';
-                    $(this).find('a.dropdown-toggle').attr('data-toggle', 'dropdown');
-                });
-                 
-                 
-                 sizeStatus = 'big';
-                }
-            
-        }
-    	                                                
+		});
+	} else if (currentSize > breakpt && sizeStatus == 'small') {
+
+		$('#mobile-menu .navbar-collapse').each(function(index) {
+			$(this).removeClass('menu' + index);
+			$(this).detach().appendTo('.menu' + index + '-parent');
+			$(this).parent().removeClass('menu' + index + '-parent');
+			sizeStatus = 'small';
+			$(this).find('a.dropdown-toggle').attr('data-toggle', 'dropdown');
+		});
+
+		sizeStatus = 'big';
+	}
+
+}
+
+/* Dropdown menu edge detection
+ *******************************/
+$(".dropdown-menu li").has("ul").hover(function(e) {
+	var elm = $('ul:first', this);
+
+	var off = elm.offset();
+	var l = off.left;
+
+	var w = elm.width();
+	var docW = $("#page").width();
+	var isEntirelyVisible = (l + w <= docW);
+
+	if (!isEntirelyVisible) {
+		$(this).addClass('edge');
+	} else {
+		var menu = $(this);
+		setTimeout(function() {
+			$(menu).removeClass('edge');
+		}, 1000);
+
+	}
+}, function(e) {
+	// on mouse out action...
+
+});
+
 /* Add UNM Panel
-***********************/
-
+ ***********************/
 $(function() {
 	jQuery("body").append('<div id="unm_panel" class="hidden-xs hidden-sm"><div class="container"></div></div>');
 	jQuery("#toolbar-nav").append('<li class="unm_panel_open hidden-sm"><a href="#panel">more <span class="caret"></span></a></li>');
-	    
+
 	jQuery('.unm_panel_open').click(function(event) {
-	    event.preventDefault();
-	    $( '.unm_panel_open .caret' ).toggleClass( "up" );
-	    $( "#unm_panel" ).slideToggle('slow');       
-	}); 
+		event.preventDefault();
+		$('.unm_panel_open .caret').toggleClass("up");
+		$("#unm_panel").slideToggle('slow');
+	});
 });
- 
+
 /* Panel JSON Test
-**********************/
+ **********************/
 /* Loading JSON objects using JSONP */
+
 (function($) {
-    
-    var url = 'http://webcore.unm.edu/json.php';
-    $.ajax({
-       type: 'GET',
-        url: url,
-        async: false,
-        contentType: "application/json",
-        dataType: 'jsonp',
-        data: { content: "v2/unm-panel.html"},
-        jsonpCallback: "JsonCallBack",
-       
-        error: function( xhr,err,exception, status) {
-            console.log( 'Sample of error data:', err );
-            console.log(status +" "+exception);
-            console.log("readyState: "+xhr.readyState+"\nstatus: "+xhr.status+"\nresponseText: "+xhr.responseText);
-    }    
-    });
+
+	var url = 'http://webcore.unm.edu/json.php?content=v2/unm-panel.html';
+
+	$.getJSON(url).done(function(data) {
+		$("#unm_panel .container").append(data.content);
+		console.log("JSON Data...");
+	}).fail(function(xhr, err, exception, status) {
+		console.log('Error data:', err);
+		console.log(status + " " + exception);
+		console.log("readyState: " + xhr.readyState + "\nstatus: " + xhr.status + "\nresponseText: " + xhr.responseText);
+	});
+});
+
+/* pre CORS version - delete after verification of new setup
+$.ajax({
+type: 'GET',
+url: url,
+async: false,
+contentType: "application/json",
+dataType: 'jsonp',
+data: { content: "v2/unm-panel.html"},
+jsonpCallback: "JsonCallBack",
+
+error: function( xhr,err,exception, status) {
+console.log( 'Sample of error data:', err );
+console.log(status +" "+exception);
+console.log("readyState: "+xhr.readyState+"\nstatus: "+xhr.status+"\nresponseText: "+xhr.responseText);
+}
+});
+
 })(jQuery);
 
 function JsonCallBack(data){
-    //It's really here
-    $("#unm_panel .container").append(data.content);
+//It's really here
+$("#unm_panel .container").append(data.content);
 }
-
+END pre CORS version  */
 
 /**********Load Lobo Alerts  *******/
 /* Loading JSON objects using JSONP */
 (function($) {
-    
-    var url = 'https://webcore.unm.edu/v2/loboalerts.json';
-    $.ajax({
-       type: 'GET',
-        url: url,
-        async: false,
-        contentType: "application/json",
-        dataType: 'jsonp',
-        jsonpCallback: "LoboAlertCallBack",
-       
-        error: function( xhr,err,exception, status) {
-            console.log( 'Sample of error data:', err );
-            console.log(status +" "+exception);
-            console.log("readyState: "+xhr.readyState+"\nstatus: "+xhr.status+"\nresponseText: "+xhr.responseText);
-    }    
-    });
+	var url = 'http://webcore.unm.edu/v2/loboalerts.json';
+	$.getJSON(url).done(function(data) {
+		if (data.alert != 'none') {
+			$(".navbar-unm").after('<div id="loboalert" class="alert alert-danger row"><span class="fa fa-warning col-md-1"> </span><div class="content col-md-11"></div></div>');
+			$("#loboalert .content").append('<hgroup><h2>' + data.alert + '</h2><h3>' + data.date + '</h3></hgroup>');
+			$("#loboalert .content").append('<p>' + data.details + ' <a href="' + data.link + '">Read More</a></p>');
+		} else {
+			console.log('No Active LoboAlerts');
+		}
+	}).fail(function(xhr, err, exception, status) {
+		console.log('Error data:', err);
+		console.log(status + " " + exception);
+		console.log("readyState: " + xhr.readyState + "\nstatus: " + xhr.status + "\nresponseText: " + xhr.responseText);
+	});
+});
+
+/* pre CORS version - delete after verification of new setup
+$.ajax({
+type: 'GET',
+url: url,
+async: false,
+contentType: "application/json",
+dataType: 'jsonp',
+jsonpCallback: "LoboAlertCallBack",
+
+error: function( xhr,err,exception, status) {
+console.log( 'Sample of error data:', err );
+console.log(status +" "+exception);
+console.log("readyState: "+xhr.readyState+"\nstatus: "+xhr.status+"\nresponseText: "+xhr.responseText);
+}
+});
 })(jQuery);
 
 function LoboAlertCallBack(data){
-    //It's really here
-    if (data.alert != 'none') {
-	    $(".navbar-unm").after('<div id="loboalert" class="alert alert-danger row"><span class="fa fa-warning col-md-1"> </span><div class="content col-md-11"></div></div>');
-	    $("#loboalert .content").append('<hgroup><h2>' + data.alert + '</h2><h3>' + data.date + '</h3></hgroup>');
-	    $("#loboalert .content").append('<p>' + data.details + ' <a href="' + data.link + '">Read More</a></p>');
-   }
+//It's really here
+if (data.alert != 'none') {
+$(".navbar-unm").after('<div id="loboalert" class="alert alert-danger row"><span class="fa fa-warning col-md-1"> </span><div class="content col-md-11"></div></div>');
+$("#loboalert .content").append('<hgroup><h2>' + data.alert + '</h2><h3>' + data.date + '</h3></hgroup>');
+$("#loboalert .content").append('<p>' + data.details + ' <a href="' + data.link + '">Read More</a></p>');
 }
+}
+END pre CORS version  */
 
-
-
-
-
+/********** Scroll to Top *******/
+$("#totop").click(function() {
+	$("html, body").animate({
+		scrollTop : 0
+	}, "slow");
+	return false;
+});
+$(window).scroll(function() {
+	if ($(window).scrollTop() > 200) {
+		$('#totop').fadeIn();
+	} else {
+		$('#totop').fadeOut();
+	}
+});
 
