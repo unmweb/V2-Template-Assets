1f559c7a1d4cd63c4bbd26f71d0d30ae186bdcd1
Matt Carter <mdcarter@unm.edu>
2015-03-06 11:42:46 -0700

    reordered bootstrap js files to match load order

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,18 +20,18 @@ module.exports = function(grunt) {
 		    dist: {
 		        src: [
 		        	  'bower_components/jquery/dist/jquery.js', //JQuery
-			          'bower_components/bootstrap/js/affix.js',
+		        	  'bower_components/bootstrap/js/transition.js',
 			          'bower_components/bootstrap/js/alert.js',
 			          'bower_components/bootstrap/js/button.js',
 			          'bower_components/bootstrap/js/carousel.js',
 			          'bower_components/bootstrap/js/collapse.js',
 			          'bower_components/bootstrap/js/dropdown.js',
-			          'bower_components/bootstrap/js/modal.js',
+			          'bower_components/bootstrap/js/modal.js',			          
+			          'bower_components/bootstrap/js/tooltip.js',	
 			          'bower_components/bootstrap/js/popover.js',
 			          'bower_components/bootstrap/js/scrollspy.js',
-			          'bower_components/bootstrap/js/tab.js',
-			          'bower_components/bootstrap/js/tooltip.js',
-		        	  'bower_components/bootstrap/js/transition.js',
+			          'bower_components/bootstrap/js/tab.js',			          		          
+			          'bower_components/bootstrap/js/affix.js',
 			          'src/js/unm-scripts.js', 	
 		        ],
 		        dest: 'dist/js/unm-scripts.js',
@@ -57,8 +57,12 @@ module.exports = function(grunt) {
                 files: {
                     "dist/css/unm-styles.css": [
                     	"bower_components/fontawesome/less/font-awesome.less", // Font Awesome
-                    	"src/less/unm-styles.less"  // Bootstrap and UNM Styles in one
-                      ]	
+                    	"src/less/unm-styles.less",  // Bootstrap and UNM Styles in one
+                      ],
+                    "dist/css/unm-hsc-styles.css": [
+                    	"bower_components/fontawesome/less/font-awesome.less", // Font Awesome
+                    	"src/less/unm-hsc-styles.less",  // Bootstrap and UNM Styles in one
+                      ],                    	
                 }
             },
 
@@ -72,8 +76,12 @@ module.exports = function(grunt) {
                 files: {
                     "dist/css/unm-styles.min.css": [
                     	"bower_components/fontawesome/less/font-awesome.less", // Font Awesome
-                    	"src/less/unm-styles.less"  // Bootstrap and UNM Styles in one
-                      ]	
+                    	"src/less/unm-styles.less",  // Bootstrap and UNM Styles in one
+                      ],	
+                    "dist/css/unm-hsc-styles.min.css": [
+                    	"bower_components/fontawesome/less/font-awesome.less", // Font Awesome
+                    	"src/less/unm-hsc-styles.less",  // Bootstrap and UNM Styles in one
+                      ],
                 }
             }
         },
